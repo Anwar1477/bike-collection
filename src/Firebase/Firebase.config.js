9140af93bed1a64ae610438ef74587a4d02bd434
Anwar1477 <joy.cse017@gmail.com>
2022-10-03 14:59:48 +0600

    done

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyACp2CThsksK8aFWLeJCT-_M98Es1DMjNw",
+  authDomain: "biker-hub.firebaseapp.com",
+  projectId: "biker-hub",
+  storageBucket: "biker-hub.appspot.com",
+  messagingSenderId: "56507763358",
+  appId: "1:56507763358:web:9c97ced36a981ec4b9898f",
+};
+
+export default firebaseConfig;
